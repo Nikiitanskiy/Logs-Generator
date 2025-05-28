1ae4d421e26957f0e2bc1e97c7db62a75f63fc65
Nikiitanskiy <nik1308958@gmail.com>
2025-05-28 22:53:14 +0300

    Reworking

--- logs_generator.py
+++ logs_generator.py
@@ -12,42 +12,51 @@ class Logs:
 
     def run(self):
         """Method to start generating logs"""
-        self.generate_logs()
+        self.__generate_logs()
 
 
 class HTTP_logs(Logs):
     """Class to generate HTTP data"""
-    def __init__(self):
-        pass
+    def __init__(self, get_resources, post_resources):
+        if get_resources:
+            self.GET_resources = get_resources
+        else:
+            self.GET_resources = ["https://httpbin.org/get","https://picsum.photos/200/300", "https://jsonplaceholder.typicode.com/posts", "https://postman-echo.com/get", "https://8.8.8.8" ]
+        
+        if post_resources:
+            self.POST_resources = post_resources
+        else:
+            POST_resources = ["https://httpbin.org/post", "https://jsonplaceholder.typicode.com/posts"]
 
 
     def __generate_logs(self):
         """Function to generate request with random resources"""
-        GET_resources = ["https://httpbin.org/get","https://picsum.photos/200/300", "https://jsonplaceholder.typicode.com/posts", "https://postman-echo.com/get", "https://8.8.8.8" ]
-        POST_resources = ["https://httpbin.org/post", "https://jsonplaceholder.typicode.com/posts"]
+
         choice_pair = ['GET', 'POST']
         while True:
             choice = random.choice(choice_pair)
             if choice == "GET":
-                gen = requests.get(random.choice(GET_resources))
+                gen = requests.get(random.choice(self.GET_resources))
             else:
-                gen = requests.post(random.choice(POST_resources))
+                gen = requests.post(random.choice(self.POST_resources))
             print(gen.status_code)
             time.sleep(0.2)
 
 
 class ICMP_logs(Logs):
     """Class to generate ICMP data"""
-    def __init__(self):
-        pass
+    def __init__(self, resources):
+
+        if resources:
+            self.PING_resources = resources
+        else:
+            self.PING_resources = ["8.8.8.8", "1.0.0.1", "8.8.4.4", "cloudflare-dns.com", "google.com"]
 
     def __generate_logs(self):
-        """Function to generate request with random resources"""
+        """Function to generate logs with  resources"""
 
-        PING_resources = ["8.8.8.8", "1.0.0.1", "8.8.4.4", "cloudflare-dns.com", "google.com"]
-        
         while True:
-            random_res = random.choice(PING_resources)
+            random_res = random.choice(self.PING_resources)
             host = ping(random_res,privileged=False)
             print(f"ICMP data to {random_res} : Sent:{host.packets_sent} Received : {host.packets_received} Loss : {host.packet_loss}")
 
