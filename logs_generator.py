26b3d0cd8ce9a39c5fc9992d1d4d6babddd91ca9
Nikiitanskiy <nik1308958@gmail.com>
2025-05-28 22:58:41 +0300

    Fix BUUUUGS

--- logs_generator.py
+++ logs_generator.py
@@ -12,24 +12,24 @@ class Logs:
 
     def run(self):
         """Method to start generating logs"""
-        self.__generate_logs()
+        self._generate_logs()
 
 
 class HTTP_logs(Logs):
     """Class to generate HTTP data"""
-    def __init__(self, get_resources, post_resources):
-        if get_resources:
+    def __init__(self, get_resources=0, post_resources=0):
+        if get_resources != 0:
             self.GET_resources = get_resources
         else:
             self.GET_resources = ["https://httpbin.org/get","https://picsum.photos/200/300", "https://jsonplaceholder.typicode.com/posts", "https://postman-echo.com/get", "https://8.8.8.8" ]
         
-        if post_resources:
+        if post_resources != 0:
             self.POST_resources = post_resources
         else:
-            POST_resources = ["https://httpbin.org/post", "https://jsonplaceholder.typicode.com/posts"]
+            self.POST_resources = ["https://httpbin.org/post", "https://jsonplaceholder.typicode.com/posts"]
 
 
-    def __generate_logs(self):
+    def _generate_logs(self):
         """Function to generate request with random resources"""
 
         choice_pair = ['GET', 'POST']
@@ -45,14 +45,14 @@ class HTTP_logs(Logs):
 
 class ICMP_logs(Logs):
     """Class to generate ICMP data"""
-    def __init__(self, resources):
+    def __init__(self, resources=0):
 
-        if resources:
+        if resources != 0:
             self.PING_resources = resources
         else:
             self.PING_resources = ["8.8.8.8", "1.0.0.1", "8.8.4.4", "cloudflare-dns.com", "google.com"]
 
-    def __generate_logs(self):
+    def _generate_logs(self):
         """Function to generate logs with  resources"""
 
         while True:
@@ -61,15 +61,15 @@ class ICMP_logs(Logs):
             print(f"ICMP data to {random_res} : Sent:{host.packets_sent} Received : {host.packets_received} Loss : {host.packet_loss}")
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
     
-#     http = HTTP_logs()
-#     icmp = ICMP_logs()
-#     t1 = threading.Thread(target=http.run)
-#     t2 = threading.Thread(target=icmp.run)
+    http = HTTP_logs()
+    icmp = ICMP_logs()
+    t1 = threading.Thread(target=http.run)
+    t2 = threading.Thread(target=icmp.run)
 
-#     t1.start()
-#     t2.start()
+    t1.start()
+    t2.start()
 
-#     t1.join()
-#     t2.join()
+    t1.join()
+    t2.join()
